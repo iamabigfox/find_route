6e6851ab1e50a33741655332c02d6f400be3b4a0
iamabigfox <sergeyritikovv@gmail.com>
2021-02-04 03:36:29 +0300

    Добавил админку, настроил базу данных с городами, вывел их на страницу

--- travel/urls.py
+++ travel/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from travel.views import home, about
 
-urlpatterns = {
+urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cities/', include(('cities.urls', 'cities'))),
     path('home/', home),
     path('about/', about),
-}
+]
