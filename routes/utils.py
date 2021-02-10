dd1dcb7b2964518f2c27f588740c0f9cecb82be0
iamabigfox <sergeyritikovv@gmail.com>
2021-02-10 15:13:35 +0300

    Commit

--- routes/utils.py
+++ routes/utils.py
@@ -1,6 +1,18 @@
 from trains.models import Train
 
 
+def dfs_path(graph, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        if vertex in graph.keys():
+            for next_ in graph[vertex] - set(path):
+                if next_ == goal:
+                    yield path + [next_]
+                else:
+                    stack.append((next_, path + [next_]))
+
+
 def get_graph(qs):
     graph = {}
     for q in qs:
@@ -10,5 +22,56 @@ def get_graph(qs):
 
 
 def get_routes(request, form) -> dict:
-    qs = Train.objects.all()
+    context = {'form': form}
+    qs = Train.objects.all().select_related('from_city', 'to_sity')
     graph = get_graph(qs)
+    data = form.cleaned_data
+    from_city = data['from_city']
+    to_city = data['to_city']
+    cities = data['cities']
+    travelling_time = data['travelling_time']
+    all_ways = list(dfs_path(graph, from_city.id, to_city.id))
+    if not len(all_ways):
+        raise ValueError('Маршрута, удовлетворяющего условия не сущетсвует')
+    if cities:
+        _cities = [city.id for city in cities]
+        right_ways = []
+        for route in all_ways:
+            if all(city in route for city in _cities):
+                right_ways.append(route)
+        if not right_ways:
+            raise ValueError('Маршрут, через эти города не возможен')
+    else:
+        right_ways = all_ways
+    routes = []
+    all_trains = {}
+    for q in qs:
+        all_trains.setdefault((q.from_city_id, q.to_city_id), [])
+        all_trains[(q.from_city_id, q.to_city_id)].append(q)
+    for route in right_ways:
+        tmp = {}
+        tmp['trains'] = []
+        total_time = 0
+        for i in range(len(route) - 1):
+            qs = all_trains[(route[i], route[i + 1])]
+            q = qs[0]
+            total_time += q.travel_time
+            tmp['trains'].append(q)
+        tmp['total_time'] = total_time
+        if total_time <= travelling_time:
+            routes.append(tmp)
+    if not routes:
+        raise ValueError('Время пути больше заданного')
+    sorted_routes = []
+    if len(routes) == 1:
+        sorted_routes = routes
+    else:
+        times = list(set(r['total_time'] for r in routes))
+        times = sorted(times)
+        for time in times:
+            for route in routes:
+                if time == route['total_time']:
+                    sorted_routes.append(route)
+    context['routes'] = sorted_routes
+    context['cities'] = {'from_city': from_city.name, 'to_city': to_city.name}
+    return context
--- travel/urls.py
+++ travel/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from routes.views import home, find_routes
+from routes.views import (
+    home, find_routes, add_route, save_route, RouteListView, RouteDetailView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,8 @@ urlpatterns = [
     path('trains/', include(('trains.urls', 'trains'))),
     path('', home, name='home'),
     path('find_routes/', find_routes, name='find_routes'),
+    path('add_route/', add_route, name='add_route'),
+    path('save_route/', save_route, name='save_route'),
+    path('list/', RouteListView.as_view(), name='list'),
+    path('detail/<int:pk>/', RouteDetailView.as_view(), name='detail'),
 ]
